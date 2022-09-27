da85b192f55b38ef5b44c960f724a98cb51a597d
Asdrúbal de Elejalde <asdrubalolocco@gmail.com>
2022-09-27 15:52:59 -0300

    terminar compra

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -2,11 +2,14 @@ import React from "react"
 import ItemCount from "../ItemCount"
 import {Link} from "react-router-dom"
 import { useCartContext } from "../Cart/CartContext"
+import { useState } from "react"
 
 const ItemDetail = ({details}) => {
+    const [cuenta, setCuenta] = useState(false)
     const {addToCart} = useCartContext()
     const onAdd = (quantity) =>{
         addToCart(details, quantity)
+        setCuenta(true)
     }
     
     return (
@@ -16,8 +19,14 @@ const ItemDetail = ({details}) => {
                 <img className='imgLego' src={details.image} alt= "fto" />
                 <h2>Precio: ${details.price}</h2>
                 <p className='description'>Descripción: {details.description}</p>
-                <ItemCount stock = {5} initial = {1} onAdd = {onAdd}/>
+                
             </div>
+
+            {
+                cuenta
+                ? <Link to = "/cart/">Terminar Compra</Link>
+                : <ItemCount stock = {5} initial = {1} onAdd = {onAdd}/>
+            }
             
             <div className="botonDetail">
             <Link to = {"/product/"}>
